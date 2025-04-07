258d99cf9627ee07f4e7ad6dd996875bd94caa1f
Caleb <calebleejiale@gmail.com>
2025-04-08 02:52:05 +0800

    modified files

---
+++ Models/AssetLike.cs
@@ -0,0 +1,10 @@
+﻿using GameAssetStorage.Models;
+
+public class AssetLike
+{
+    public int Id { get; set; }
+    public int AssetId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+
+    public Asset? Asset { get; set; }
+}
