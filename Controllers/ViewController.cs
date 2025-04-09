61a7c0cbe39cdef63ca3a9f484872fcd1b34c521
Caleb <calebleejiale@gmail.com>
2025-04-10 03:45:05 +0800

    modified files

---
+++ Controllers/ViewController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameAssetStorage.Controllers
+{
+    public class ViewController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();  // This should map to Login.cshtml
+        }
+    }
+}
